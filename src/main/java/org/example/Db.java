0b4ab010a047d654bdd7f28bcc9d7d694f3c7001
emilia <memiliatunesi@gmail.com>
2025-08-27 09:58:08 -0300

    [ADD] Retorna producto que mas recaudo

--- src/main/java/org/example/Db.java
+++ src/main/java/org/example/Db.java
@@ -36,10 +36,13 @@ public class Db {
             leerFacturasDesdeCSV(conn, "src/main/resources/facturas.csv");
             leerFacturaProductoDesdeCSV(conn, "src/main/resources/factura_producto.csv");
 
+            mostrarProductoMasRecaudo(conn);
+
             conn.commit();
             conn.close();
 
             System.out.println("Datos cargados exitosamente.");
+
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -135,4 +138,36 @@ public class Db {
 
         pstmt.close();
     }
+
+    private static void mostrarProductoMasRecaudo(Connection conn) {
+        try {
+            Statement stmt = conn.createStatement();
+            String sqlMaxRecaudacion =
+                    "SELECT p.idProducto, p.nombre, SUM(fp.cantidad * p.valor) AS recaudacion " +
+                            "FROM producto p " +
+                            "JOIN factura_producto fp ON p.idProducto = fp.idProducto " +
+                            "GROUP BY p.idProducto, p.nombre " +
+                            "ORDER BY recaudacion DESC " +
+                            "FETCH FIRST ROW ONLY";
+
+            var rs = stmt.executeQuery(sqlMaxRecaudacion);
+
+            if (rs.next()) {
+                System.out.println("\nProducto que más recaudó:");
+                System.out.println(
+                        rs.getInt("idProducto") + " - " +
+                                rs.getString("nombre") + " - Recaudación: " +
+                                rs.getDouble("recaudacion")
+                );
+            } else {
+                System.out.println("No hay productos vendidos.");
+            }
+
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
