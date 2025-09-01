b1289d001ea77b9c8cafa6ce0abbf75ee2664217
Candela Echazu <gcandela894@gmail.com>
2025-09-01 17:20:24 -0300

    Mensaje descriptivo de los cambios

--- src/main/java/org/example/app/Main.java
+++ src/main/java/org/example/app/Main.java
@@ -3,7 +3,6 @@ package org.example.app;
 public class Main {
     public static void main(String[] args) {
         //acá ir armando la app, es decir ir llamando
-
-
+    CrearEsquema.run();
     }
 }
