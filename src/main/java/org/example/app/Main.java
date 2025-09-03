ba34aeb1ad915a4604cdee40c534f60db0145706
emilia <memiliatunesi@gmail.com>
2025-09-03 15:29:03 -0300

    [UPDATE] Arregla lista repetida

--- src/main/java/org/example/app/Main.java
+++ src/main/java/org/example/app/Main.java
@@ -1,11 +1,9 @@
 package org.example.app;
 
 import org.example.dao.factory.DAOFactory;
-import org.example.dao.factory.DerbyDAOFactory;
 import org.example.dao.interfaces.ClienteDAO;
 import org.example.entity.Cliente;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -17,22 +15,6 @@ public class Main {
         DevolverMaxRecaudacion.run();
         ClienteConMasFacturas.run();
 
-
-        DAOFactory factory = DAOFactory.getDAOFactory(DAOFactory.DERBY_JDBC);
-        if (factory == null) {
-            throw new RuntimeException("No se pudo crear la factory para Derby");
-        }
-
-        ClienteDAO clienteDAO = factory.getClientDAO();
-        if (clienteDAO == null) {
-            throw new RuntimeException("No se pudo crear el ClienteDAO");
-        }
-
-        List<Cliente> lista = clienteDAO.obtenerTodos();
-
-        for (Cliente c : lista) {
-            System.out.println(c.getIdClient() + " - " + c.getNombre() + " - " + c.getEmail());
-        }
         try {
             java.sql.DriverManager.getConnection("jdbc:derby:;shutdown=true");
         } catch (SQLException e) {
